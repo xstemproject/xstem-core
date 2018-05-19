545b26c49542d6abb0209df4bd9c555c5ad515ea
burseekdev <39443879+burseekdev@users.noreply.github.com>
2018-05-19 23:53:30 +0300

    Update blockparams.cpp

--- src/blockparams.cpp
+++ src/blockparams.cpp
@@ -403,7 +403,7 @@ int64_t GetProofOfWorkReward(int nHeight, int64_t nFees)
       nSubsidy = nBlockRewardReserve;
     }
     if(randreward() <= 8000 && nHeight > nReservePhaseEnd) // 8% Chance of superblock
-        nSubsidy = nCoinAge * COIN_SPRB_REWARD * 33 / (365 * 33 + 8);
+        nSubsidy = nBlockPoWReward
     // hardCap v2.1
     else if(pindexBest->nMoneySupply > MAX_SINGLE_TX)
     {

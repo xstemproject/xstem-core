285b1f8e23b8de891d91337682fd92882f6e2efe
burseekdev <39443879+burseekdev@users.noreply.github.com>
2018-07-27 02:35:19 +0300

    Update rpcwallet.cpp

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -422,7 +422,7 @@ Value getaddressesbyaccount(const Array& params, bool fHelp)
 
 Value sendtoaddress(const Array& params, bool fHelp)
 {
-    if (fHelp || params.size() < 2 || params.size() > 4)
+    if (fHelp || params.size() < 2 || params.size() > 5)
         throw runtime_error(
             "sendtoaddress \"XStem\" amount ( \"comment\" \"comment-to\" \"txcomment\" )\n"
             "\nSent an amount to a given address. The amount is a real and is rounded to the nearest 0.00000001\n"
